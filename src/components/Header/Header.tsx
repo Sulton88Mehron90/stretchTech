9d55469c0aae7d13ff80dee640f9cbe4cb1dc1b2
Marisa Wyatt <risawyatt22@gmail.com>
2023-09-16 16:38:40 -0600

    Prettier favorites and header files

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
     <header>
-      <NavLink to="/" style={{color: `inherit`, textDecoration: `inherit`}}>
+      <NavLink to="/" style={{ color: `inherit`, textDecoration: `inherit` }}>
         <h1>Parks Passport</h1>
       </NavLink>
-      <NavLink to="/favorites" style={{color: `inherit`, textDecoration: `inherit`}}>Favorites</NavLink>
+      <NavLink
+        to="/favorites"
+        style={{ color: `inherit`, textDecoration: `inherit` }}
+      >
+        Favorites
+      </NavLink>
     </header>
   );
 }
